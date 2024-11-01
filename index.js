941ad4316ee9d438a9abbeaaafed948b42b49a9f
neomoth <69372252+neomoth@users.noreply.github.com>
2024-11-01 18:21:29 -0400

    fix timestamp

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ client.on(Events.GuildMemberUpdate, async (oldMember, newMember) => {
 	if(!oldMember.communicationDisabledUntil&&newMember.communicationDisabledUntil){
 		logEmbed.setTitle('Member muted');
 		logEmbed.setAuthor({name:oldMember.user.tag,iconURL:oldMember.user.displayAvatarURL()});
-		logEmbed.setDescription(`Timed out until <t:${newMember.communicationDisabledUntilTimestamp}:f>`);
+		logEmbed.setDescription(`Timed out until <t:${newMember.communicationDisabledUntilTimestamp/100}:f>`);
 		logEmbed.setTimestamp(Date.now());
 		logEmbed.setFooter({text:`User ID: ${oldMember.user.id}`});
 		await logChannel.send({embeds:[logEmbed]});
